075e480417b285ca4e43699cd7a8b54b8036802b
Vincent Weevers <mail@vincentweevers.nl>
2021-08-01 17:20:48 +0200

    Build to dist/

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http')
 var url = require('url')
 var send = require('send')
+var path = require('path')
 
 var port = process.argv[2] || 80
 
@@ -24,11 +25,11 @@ http.createServer(function(req, res) {
     u = '/index.html'
   }
 
-  send(req, u, { root: __dirname + '/assets/static' })
+  send(req, u, { root: path.join(__dirname, 'dist') })
     .on('error', error)
     .on('directory', redirect)
     .pipe(res);
 
 }).listen(port, function() {
-  console.log('Server running on port %d...', port)
+  console.log(`Open http://localhost:${port}`)
 })
